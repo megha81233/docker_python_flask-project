623a57e9ecab326d3b0f8a0e184f1ed77b4f144a
megha81233 <meghakondapure485@gmail.com>
2025-03-25 11:25:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask sample application on azure app service"
+    return "Flask pagalmsample application on azure app service"
 
 
 if __name__ == "__main__":
